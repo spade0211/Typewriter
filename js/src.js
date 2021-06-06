aa395d29252679c9981ef8c43c0fc20c8e4254c8
Aryan Mohakud <bbps06@gmail.com>
2021-06-06 06:43:03 +0530

    Add files via upload
    The main index.html, style.css and src.js files along with favicon images

---
+++ js/src.js
@@ -0,0 +1,13 @@
+window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: 'smooth'
+});
+setTimeout(() => {
+    window.scrollTo({
+        top: 879,
+        left: 0,
+        behavior: 'smooth'
+    });
+    document.body.style.overflow = "auto";
+}, 21700);
