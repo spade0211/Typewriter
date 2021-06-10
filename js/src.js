9befb34846fc48464a5face12f7d5cab1d9db470
Aryan Mohakud <aryanmohakud@Aryans-MacBook-Air.local>
2021-06-10 20:01:38 +0530

    styled skip button

--- js/src.js
+++ js/src.js
@@ -12,8 +12,14 @@ skills = setTimeout(() => {
     document.body.style.overflow = "auto";
 }, 21700);
 document.querySelector("button").addEventListener("click", () => {
-    document.querySelectorAll('p').forEach((i) => i.classList.add('skip'));
-    document.querySelectorAll('h1')[1].classList.add('skip');
+    document.querySelectorAll('p').forEach((i) => i.classList.add('remove'));
+    document.querySelectorAll('h1')[1].classList.add('remove');
     clearTimeout(skills);
     document.querySelector('body').style.overflow = 'auto';
+    // document.querySelector('button').classList.add('invis');
+    document.querySelector('button').classList.add('success');
+    setTimeout(() => {
+        document.querySelector('button').classList.add('invis');
+        document.querySelector('button').style.cursor = "inherit";
+    }, 3000)
 })
